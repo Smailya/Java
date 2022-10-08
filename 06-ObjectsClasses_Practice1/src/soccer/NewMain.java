c20877b21135e2dda12a5a399cacb84b99068b07
Ismail cisse <101877771+Smailya@users.noreply.github.com>
2022-10-08 16:21:02 +0200

    Add files via upload

---
+++ 06-ObjectsClasses_Practice1/src/soccer/NewMain.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soccer;
+
+/**
+ *
+ * @author ISMAIL
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           /*  Player  p1 = new Player();
+        Player  p2 = new Player();
+        Player  p3 = new Player();
+        
+        //assigning values to the objects
+        p1.playerName = "George Eliot";
+        p2.playerName = "Graham Greene";
+        p3.playerName = "Geoffrey Chaucer";
+        
+        
+        //declaring player array
+        Player [] thePlayers = {p1, p2,p3};
+        
+        //declaring team object
+        Team t1 = new Team();
+        t1.teamName = "The Greens";
+        t1.playerArray = thePlayers;
+        
+       /* System.out.println(t1.teamName + " Team");
+           for(Player theplayer: t1.playerArray){
+            System.out.println(theplayer.playerName);
+    
+        
+        //team 2
+        Team t2 = new Team();
+        t2.teamName = "The Reds";
+        t2.playerArray = thePlayers;
+        
+        t2.playerArray  = new Player[3];
+        t2.playerArray[0] = new Player();
+        t2.playerArray[0].playerName = "Rober Service";
+        
+        t2.playerArray[1] = new Player();
+        t2.playerArray[1].playerName = "Robbie Burns";
+        
+        t2.playerArray[0] = new Player();
+        t2.playerArray[0].playerName = "Rafael Sabatini";
+        
+        
+        
+        System.out.println("------------------------------");
+         System.out.println(t2.teamName + " Team");
+        for(Player theplayer: t1.playerArray){
+            System.out.println(theplayer.playerName);
+       
+       Game cG = new Game();
+       cG.homeTeam = t1;
+       cG.awayTeam = t2;
+       
+       Goal gl = new Goal();
+       gl.thePlayer = cG.homeTeam.playerArray[2];
+       gl.theTeam = cG.homeTeam;
+       gl.theTime = 55;
+       
+       Goal[] thegl = {gl};
+       cG.goals = thegl;
+       
+       System.out.print("Goal scored after " + 
+               cG.goals[0].theTime + " mins by "+
+               cG.goals[0].thePlayer.playerName + " of " + 
+               cG.goals[0].theTeam.teamName);
+       */
+           
+           String custName = "Steve Smith";
+        String firstName;
+        int spaceIdx;
+        StringBuilder sb = new StringBuilder();
+        
+        
+    
+        
+    //  Declare and initialize variables of type long, float, and char.
+        long l = 123456444444444789L;
+        float f = 12.455232f;
+        char c = 'G';
+        int result;
+        
+        
+        // Print the long variable.
+        
+        System.out.print(f);
+
+	// Assign the long to the int and print the int variable.
+        result = (int)l;
+        
+        System.out.println("Before casting: /n" +l);
+        System.out.println("After casting: n/" +result);
+        
+        
+    } 
+        
+      
+    
+        
+    
+}
---
+++ ex03_1_exercise/ShoppingCart.java
@@ -0,0 +1,17 @@
+package ex03_1_exercise;
+
+public class ShoppingCart {
+    public static void main (String[] args){
+        
+        String custName, itemDesc, message;
+        message = "wants to purchase";
+        custName = "Mary";
+        itemDesc = "Shirt";
+        
+        
+        
+        
+        
+        System.out.println(custName + message + itemDesc);    
+    }    
+}
