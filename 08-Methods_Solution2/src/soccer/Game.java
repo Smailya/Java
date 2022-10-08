99ea8b8c5cc49feeab7b86a703592700b68e2318
Ismail cisse <101877771+Smailya@users.noreply.github.com>
2022-10-08 16:35:45 +0200

    Add files via upload

---
+++ 08-Methods_Solution2/src/soccer/Game.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package soccer;
+
+import utility.GameUtils;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Game {
+    
+    public Team homeTeam;
+    public Team awayTeam;
+    public Goal[] goals;
+    
+    public void playGame() {
+        
+        int numberOfGoals = (int)(Math.random() * 7);
+        
+        Goal[] theGoals = new Goal[numberOfGoals];
+        this.goals = theGoals;
+        GameUtils.addGameGoals(this);        
+    }
+    
+    public String getDescription() {
+        StringBuilder returnString = new StringBuilder();
+        for (Goal currGoal: this.goals) {
+            returnString.append("Goal scored after "
+            + currGoal.theTime + " mins by "
+            + currGoal.thePlayer.playerName + " of "
+            + currGoal.theTeam.teamName +
+              "\n");
+        }
+        return returnString.toString();
+    }
+      
+}
