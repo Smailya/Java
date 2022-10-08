5a9d8c37ebf1febd2a38bcc90ee1139981ff94c1
Ismail cisse <101877771+Smailya@users.noreply.github.com>
2022-10-08 16:38:46 +0200

    Add files via upload

---
+++ 09-EncapConstructors_Practice2/src/soccer/League.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soccer;
+
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class League {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        League theLeague = new League();
+
+        Team[] theTeams = theLeague.createTeams();
+        Game[] theGames = theLeague.createGames(theTeams);
+
+        Game currGame = theGames[0];
+        
+        currGame.playGame();
+        
+        System.out.println(currGame.getDescription());
+
+    }
+
+    public Team[] createTeams() {
+
+        Player player1 = new Player("Ismail Cisse");
+       // player1.setPlayerName("George Eliot");
+        Player player2 = new Player("Ibrahim Cisse");
+       // player2.setPlayerName();
+        Player player3 = new Player("Hamza Cisse");
+        //player3.setPlayerName();
+         Player player4 = new Player("Kara Cisse");
+        Player[] thePlayers = {player1, player2, player3};
+
+        Team team1 = new Team("The Greens", thePlayers);
+        /* Practice 9-2. The following two lines can be removed after the line above has been
+           modified to pass parameters to the constructor of Team */
+       
+
+        // Create team2
+        Team team2 = new Team();
+        team2.setTeamName("The Reds");
+        team2.setPlayerArray(new Player[3]);
+        team2.getPlayerArray()[0] = new Player();
+        team2.getPlayerArray()[0].setPlayerName("Robert Service");
+        team2.getPlayerArray()[1] = new Player();
+        team2.getPlayerArray()[1].setPlayerName("Robbie Burns");
+        team2.getPlayerArray()[2] = new Player();
+        team2.getPlayerArray()[2].setPlayerName("Rafael Sabatini");
+
+        Team[] theTeams = {team1, team2};
+        return theTeams;
+    }
+
+    public Game[] createGames(Team[] theTeams) {
+        Game theGame = new Game(theTeams[0], theTeams[1]);
+        Game[] theGames = {theGame};
+        return theGames;
+    }
+
+}
