c3497216661619be93131b34eb5b64cdfa114a06
Ismail cisse <101877771+Smailya@users.noreply.github.com>
2022-10-08 16:31:08 +0200

    Add files via upload

--- ex07_2_exercise/ShoppingCart.java
+++ ex07_2_exercise/ShoppingCart.java
@@ -12,16 +12,11 @@ public class ShoppingCart {
         firstName = custName.substring(0, spaceIdx);
         System.out.println(firstName);
           
-        //Instantiate and initialize sb to firstName.
-        sb = new StringBuilder(firstName);
+	//Instantiate and initialize sb to firstName.
 
 	// Put the full name back together, using StringBuilder append method.
 	//   You can just enter the String literal for the last name.
 	//   Print the full name.
-        sb.append(" Smith");
-	
-	// Challenge: sb.append(custName.substring(spaceIdx));
-
-        System.out.println(sb);
-    }    
+        
+    }
 }
